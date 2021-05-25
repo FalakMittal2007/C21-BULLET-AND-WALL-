b763fdc4bd2b187d26c3b5206468b95ff3ab591f
FalakMittal2007 <83964053+FalakMittal2007@users.noreply.github.com>
2021-05-25 10:21:58 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -38,7 +38,7 @@ function draw() {
  {
 
 var damage= 0.5*weight*speed*speed/(thickness*thickness*thickness);
-bullet.velocityX=0;
+
  if(damage>10)
 {
  wall.shapeColor=color(255,0,0);
@@ -50,6 +50,11 @@ bullet.velocityX=0;
   wall.shapeColor=color(0,255,0);
   
   }
+ }
+
+ if(wall.x-bullet.x<(bullet.width+wall.width)/2)
+ {
+ bullet.velocityX=0;
  }
  
 
